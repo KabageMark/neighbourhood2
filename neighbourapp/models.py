8931b745ef96abc25b128c674d13ec8528d5a708
kabagemark <kabagemark@gmail.com>
2018-10-20 11:26:49 +0300

    business class model added

--- neighbourapp/models.py
+++ neighbourapp/models.py
@@ -8,6 +8,9 @@ class NeighbourHood(models.Model):
     Occupants Count = models.PositiveIntegerField()
     Admin Foreign key = models.ForeignKey(User,on_delete=models.CASCADE ,null=True)
 
-# class Business(models.Model):
-#     name = models.CharField(max_length=30)
-#     id=models.PositiveIntegerField()
+class Business(models.Model):
+    user = models.ForeignKey(User,null=True)
+    name = models.CharField(max_length=30)
+    id = models.AutoField()
+    email = models.EmailField(max_length=70,null=True)
+
