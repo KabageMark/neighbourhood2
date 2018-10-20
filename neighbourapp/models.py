21e5c09f486bc901a6166c2dd8ceff7654c45f3d
kabagemark <kabagemark@gmail.com>
2018-10-20 11:04:10 +0300

    Neighbourhood class

--- neighbourapp/models.py
+++ neighbourapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class NeighbourHood(models.Model):
+    image_name = models.CharField(max_length =30)
+    image_location = models.CharField(max_length =30)
+    Occupants Count = models.PositiveIntegerField()
+    Admin Foreign key = models.ForeignKey(User,on_delete=models.CASCADE ,null=True)
+
+# class Business(models.Model):
+#     name = models.CharField(max_length=30)
+#     id=models.PositiveIntegerField()
