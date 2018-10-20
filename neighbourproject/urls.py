c049b071a15f937995ae21daf871f79a7ba024d3
kabagemark <kabagemark@gmail.com>
2018-10-20 15:46:32 +0300

    authentication system added

--- neighbourproject/urls.py
+++ neighbourproject/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'', include('neighbourapp.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^neighbourapp/',include('neighbourapp.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
