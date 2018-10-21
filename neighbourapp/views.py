1020078ca972ea085cf274e6058057f94a69cd76
kabagemark <kabagemark@gmail.com>
2018-10-21 14:06:38 +0300

    profile view function added

--- neighbourapp/views.py
+++ neighbourapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Profile,NeighbourHood,Business
 # Create your views here.
 def welcome(request):
     return render(request, 'index.html')
+
+
+def Profiles(request):
+    profile = Profile.get_all()
+    return render(request, 'profile.html',{"profile":profile,"image":image,})
